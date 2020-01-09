65fab40e7b86b3cc7d5333cf9a52a4320af084ab
ABSphreak <absphreak@outlook.com>
2020-01-09 08:30:30 +0530

    Added delete functionality

--- lib/QuoteCard.dart
+++ lib/QuoteCard.dart
@@ -4,8 +4,9 @@ import './quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
+  final Function delete;
   //Constructor
-  QuoteCard({this.quote});
+  QuoteCard({this.quote, this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +31,12 @@ class QuoteCard extends StatelessWidget {
                     fontSize: 14.0,
                     color: Colors.grey[800]
                 ),
+              ),
+              SizedBox(height: 8.0,),
+              FlatButton.icon(
+                  onPressed: delete,
+                  icon: Icon(Icons.delete),
+                  label: Text('Delete Quote')
               ),
             ],
           ),
