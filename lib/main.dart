12905aabd420775470bbade8c53a77d523dda219
ABSphreak <absphreak@outlook.com>
2020-01-09 08:01:31 +0530

    Adding Quote Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -11,10 +12,10 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'Be yourself; eveyone else is taken already!',
-    'I have nothing to declare except my genius.',
-    'The truth is rarely pure and never simple.'
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; eveyone else is taken already!'),
+  Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius.'),
+  Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple.'),
   ];
 
   @override
@@ -27,7 +28,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       )
     );
   }
---
+++ lib/quote.dart
@@ -0,0 +1,13 @@
+class Quote {
+
+  String text;
+  String author;
+
+  //Using named parameters for setting values in constructor
+  //  Quote({String text, String author}){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+  Quote({this.text, this.author});
+
+}
